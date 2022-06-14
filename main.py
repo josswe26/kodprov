fa724866feac0f9db6778d7444a0ed52e6293095
Jose Sanchez <jsanchez026@gmail.com>
2022-06-14 22:10:58 +0200

    Add start method counter

--- main.py
+++ main.py
@@ -11,8 +11,27 @@ def list_sorted_tracks(driver_data):
     return tracks
 
 
+def start_method_counter(driver_data, start_method):
+    counter = 0
+    for race_start in driver_data:
+        if race_start["startMethod"] == start_method:
+            counter += 1
+
+    return counter
+
+
+
+
+
 with open("goop.json") as file:
     goop_data = json.load(file)
 
+print("\n***** Fråga 1 *****")
+print("\nLista av travbanor i alfabetisk ordning:\n")
 for track in list_sorted_tracks(goop_data):
-    print(track)
+    print("- ", track)
+
+print("\n***** Fråga 2 *****")
+print(f"\nDet kördes {start_method_counter(goop_data, 'A')} med Autostart.")
+
+
